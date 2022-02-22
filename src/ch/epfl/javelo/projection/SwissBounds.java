b3171c740aa9710df01e4c3209c9e6cfcb5c9a56
Loris Verga <loris.verga@epfl.ch>
2022-02-22 09:39:44 +0100

    Classe SwissBounds finie

---
+++ src/ch/epfl/javelo/projection/SwissBounds.java
@@ -0,0 +1,50 @@
+package ch.epfl.javelo.projection;
+
+/**
+ * Classe SwissBounds
+ *
+ * Cette méthode non instantiable contient des constantes et des méthodes liées aux limites de la Suisse.
+ *
+ * @author Loris Verga (345661)
+ */
+public final class SwissBounds {
+
+    //Liste de constantes utiles :
+
+    //la plus petite coordonnée E de Suisse
+    final static double MIN_E = 2485000;
+
+    //la plus grande coordonnée E de Suisse
+    final static double MAX_E = 2834000;
+
+    //la plus petite coordonnée N de Suisse
+    final static double MIN_N = 1075000;
+
+    //la plus grande coordonnée N de Suisse
+    final static double MAX_N = 1296000;
+
+    //la largeur de la Suisse en mètres, définie comme la différence entre MAX_E et MIN_E
+    final static double WIDTH = MAX_E-MIN_E;
+
+    //la hauteur de la Suisse en mètres, définie comme la différence entre MAX_N et MIN_N.
+    final static double HEIGHT = MAX_N-MIN_N;
+
+
+    /**
+     * Méthode containsEN : Cette méthode publique et statique permet de tester si un point (des coordonnées) se trouve
+     * dans les limites de la Suisse.
+     * @param e coordonnée Est du point dont on veut déterminer s'il se trouve en Suisse
+     * @param n coordonnée Nord du point dont on veut déterminer s'il se trouve en Suisse
+     * @return true si le point se trouve en Suisse
+     */
+    public static boolean containsEN(double e, double n){
+
+        if (e > MIN_E && e < MAX_E && n > MIN_N && n < MAX_N) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+}
