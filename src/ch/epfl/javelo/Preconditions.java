b6768899db92463ab2a6f18254429d67ceb5f30f
Loris Verga <loris.verga@epfl.ch>
2022-02-22 09:15:51 +0100

    Classe Preconditions finie

--- src/ch/epfl/javelo/Preconditions.java
+++ src/ch/epfl/javelo/Preconditions.java
@@ -1,5 +1,29 @@
 package ch.epfl.javelo;
+
+
+/**
+ * Classe Preconditions
+ *
+ * @author Loris Verga (3455661)
+ *
+ */
 public final class Preconditions {
     private Preconditions() {}
-    // … méthodes
+
+    /**
+     * Méthode checkArgument: permet de vérifier la validité des arguments d'une méthode.
+     *
+     * @param shouldBeTrue correspond à la condition pour que le paramètre d'une méthode soit accepté,
+     *                     si cette condition n'est pas respectée, la méthode checkArgument enverra une IllegalArgumentException
+     *
+     * Exemple d'utilisation : Preconditions.checkArgument(array.length > 0) ;
+     */
+    public static void checkArgument(boolean shouldBeTrue) {
+
+        if (!shouldBeTrue) {
+            throw new IllegalArgumentException();
+            //exception de type unchecked
+        }
+
+    }
 }
