13021ba5416821070782bd12cd60d585c527d0be
Loris Verga <loris.verga@epfl.ch>
2022-02-25 12:41:03 +0100

    début PointWebMercator

---
+++ src/ch/epfl/javelo/projection/PointWebMercator.java
@@ -0,0 +1,11 @@
+package ch.epfl.javelo.projection;
+
+
+/**
+ * Classe record PointWebMercator: Cette classe représente un point dans le système Web Mercator
+ *
+ * @author Loris Verga (345661)
+ */
+public record PointWebMercator(double x, double y) {
+}
+
