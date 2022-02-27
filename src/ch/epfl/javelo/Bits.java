c1e0e6bb35b8bc6d221cd86e6c1c916d5f75e8bc
Juanika-chr <juan.iaconucci@gmail.com>
2022-02-27 17:13:47 +0100

    Bits and TestBits finished!!

--- src/ch/epfl/javelo/Bits.java
+++ src/ch/epfl/javelo/Bits.java
@@ -10,29 +10,66 @@ package ch.epfl.javelo;
 public final class Bits {
     private Bits() {}
 
+    /**
+     * Retourne depuis value, la plage de length bits commençant au bit d'index start,
+     * en l'interprétant comme valeur signée en complément a deux
+     *
+     * @param value vecteur de 32 bits qu'on veut extraire une valeur
+     * @param start le début de la plage de bit qu'on veut extraire
+     * @param length la longueur de la plage de bit qu
+     * @return la plage de length bits
+     */
     public static int extractSigned(int value, int start, int length){
-        Preconditions.checkArgument(0<=start && start<=31 && 0<=start+length && start+length<=31);
-        int leftShift = start + length;
+        Preconditions.checkArgument(0<=start && start<=31 && 0<=start+length && start+length<=32 && length>=0);
+
+        //todo ask for this, how does >>> work and why doesn't it change the value??
+        if (length == 0){return 0;}
+
+        int leftShift = 32 - (start + length);
         int rightShift = start + leftShift;
-        int newValue;
-        //todo go to course, archive, week 10, look at videos about bits
+
+        int leftShiftedValue;
+        int finalValue;
 
         //Décalage de la plage de bit vers la gauche
-        newValue = (int) (value * Math.pow(10 , leftShift));
+        leftShiftedValue = value << leftShift;
 
-        //ejectage des bits supplementaire
-        newValue = (int) (newValue % Math.pow(10 , 32));
+        //Décalage arithmétique de la plage de bit vers la droite
+        finalValue = leftShiftedValue >> rightShift;
 
-        //Décalage de la plage de bit vers la droite et ejectage des bits supplementaire
-        newValue = (int) Math.floor(newValue / (int)(Math.pow(10 , rightShift)));
-
-        return newValue;
+        return finalValue;
     }
 
+    /**
+     * Retourne depuis value, la plage de length bits commençant au bit d'index start,
+     * en l'interprétant comme valeur non signée en complément a deux
+     *
+     * @param value vecteur de 32 bits qu'on veut extraire une valeur
+     * @param start le début de la plage de bit qu'on veut extraire
+     * @param length la longueur de la plage de bit qu
+     * @return la plage de length bits
+     */
     public static int extractUnsigned(int value, int start, int length){
 
+        //todo ask about length=32 in this example
+        Preconditions.checkArgument(0<=start && start<=31 && 0<=start+length && start+length<=32 && length>=0 && length<32);
 
-        return value;
+        if (length == 0){return 0;}
+
+        int leftShift = 32 - (start + length);
+        int rightShift = start + leftShift;
+
+        int leftShiftedValue;
+        int finalValue;
+
+        //Décalage de la plage de bit vers la gauche
+        leftShiftedValue = value << leftShift;
+
+        //Décalage logique de la plage de bit vers la droite
+        finalValue = leftShiftedValue >>> rightShift;
+
+        return finalValue;
+
     }
 
 }
--- src/ch/epfl/javelo/BitsTest.java
+++ src/ch/epfl/javelo/BitsTest.java
@@ -45,7 +45,7 @@ class BitsTest {
     @Test
     void extractSignedWorksOnKnownValues(){
 
-        var actual1 = Bits.extractSigned(0b11001010111111101011101010111110, 0,0);
+        var actual1 = Bits.extractSigned(0b11001010111111101011101010111110, 7,0);
         var expected1 = 0b0;
         assertEquals(expected1,actual1);
 
@@ -57,7 +57,7 @@ class BitsTest {
         var expected3 = 0b11111111111111111111111111111110;
         assertEquals(expected3,actual3);
 
-        var actual4 = Bits.extractSigned(0b11001010111111101011101010111110, 18,4);
+        var actual4 = Bits.extractSigned(0b11001010111111101011101010111110, 28,4);
         var expected4 = 0b11111111111111111111111111111100;
         assertEquals(expected4,actual4);
 
@@ -120,13 +120,13 @@ class BitsTest {
         var expected3 = 0b1110;
         assertEquals(expected3,actual3);
 
-        var actual4 = Bits.extractUnsigned(0b11001010111111101011101010111110, 18,4);
+        var actual4 = Bits.extractUnsigned(0b11001010111111101011101010111110, 28,4);
         var expected4 = 0b1100;
         assertEquals(expected4,actual4);
 
         //todo check if this is correct
-        var actual5 = Bits.extractUnsigned(0b11001010111111101011101010111110, 0,32);
-        var expected5 = 0b11001010111111101011101010111110;
+        var actual5 = Bits.extractUnsigned(0b11001010111111101011101010111110, 0,31);
+        var expected5 = 0b01001010111111101011101010111110;
         assertEquals(expected5,actual5);
     }
 
