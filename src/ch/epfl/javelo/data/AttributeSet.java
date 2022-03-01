229458869c819821a461063ff4d77c94b5a7fb6b
Juanika-chr <juan.iaconucci@gmail.com>
2022-03-01 08:12:26 +0100

    Q_28_4 finished and tested

---
+++ src/ch/epfl/javelo/data/AttributeSet.java
@@ -0,0 +1,11 @@
+package ch.epfl.javelo.data;
+
+/**
+ * L'enregistrement AttributeSet représente un ensemble d'attributs OpenStreetMap
+ *
+ * @author Juan B Iaconucci (342153)
+ */
+public record AttributeSet() {
+
+    //public AttributeSet(int ){}
+}
