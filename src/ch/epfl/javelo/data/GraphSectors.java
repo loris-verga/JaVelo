51c3706dcedbae6a116e08884d559b77cb550521
Juanika-chr <juan.iaconucci@gmail.com>
2022-03-05 13:46:09 +0100

    "finished" GraphSectors I think.. need to test

--- src/ch/epfl/javelo/data/GraphSectors.java
+++ src/ch/epfl/javelo/data/GraphSectors.java
@@ -1,12 +1,10 @@
 package ch.epfl.javelo.data;
 
 import ch.epfl.javelo.projection.PointCh;
+import ch.epfl.javelo.projection.SwissBounds;
 
 import java.nio.ByteBuffer;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 /** GraphSectors représente le tableau contenant les 16384 secteurs de JaVelo
  *
@@ -14,12 +12,47 @@ import java.util.ListIterator;
  */
 public record GraphSectors(ByteBuffer buffer) {
 
-    //todo not sure about this
+    private static final int OFFSET_ID_OF_FIRST_HALF_NODE = 0;
+    private static final int OFFSET_ID_OF_SECOND_HALF_NODE = OFFSET_ID_OF_FIRST_HALF_NODE + 1;
+    private static final int OFFSET_NB_OF_NODE = OFFSET_ID_OF_SECOND_HALF_NODE + 1;
+    private static final int SECTOR_INTS = OFFSET_NB_OF_NODE + 1;
+
     record Sector(int startNodeId, int endNodeId){}
 
+    /**
+     * Methode qui retourne une liste contenant tous les sectors qui sont incluent dans le carré de cotée le double de distance
+     *
+     * @param center les coordonnées du centre du carré
+     * @param distance la moitié du coté du carré
+     * @return une liste contenant tous les sectors qui sont incluent dans le carré de cotée le double de distance
+     */
     public List<Sector> sectorsInArea(PointCh center, double distance){
-        //todo have no clue how to do this
+        //todo lots of test on this one
 
-        return null;
+        List<Sector> listOfSectorsInArea = new ArrayList<>();
+
+        double sectorLengthE = SwissBounds.WIDTH / 128.0;
+        double sectorLengthN = SwissBounds.HEIGHT / 128.0;
+
+        double newCenterE = (center.e() - SwissBounds.MIN_E) / sectorLengthE;
+        double newCenterN = (center.n() - SwissBounds.MIN_N) / sectorLengthN;
+        double newSideLengthE = distance / sectorLengthE;
+        double newSideLengthN = distance / sectorLengthN;
+
+        int infX = (int) Math.floor(newCenterE - newSideLengthE) ;
+        int infY = (int) Math.floor(newCenterN - newSideLengthN);
+        int maxX = (int) Math.ceil(newCenterE + newCenterE);
+        int maxY = (int) Math.ceil(newCenterE + newCenterN);
+
+        for (int x = infX; x < maxX; x++){
+            for (int y = infY; y < maxY; y++) {
+                int sectorId = x + y * 128;
+                int startNodeId = buffer.getShort(sectorId * SECTOR_INTS + OFFSET_ID_OF_SECOND_HALF_NODE) << 16  + buffer.getShort(sectorId * SECTOR_INTS + OFFSET_ID_OF_FIRST_HALF_NODE);
+                int nBOfNodes = buffer.getShort(sectorId * SECTOR_INTS + OFFSET_NB_OF_NODE);
+                int endNodeId = startNodeId + nBOfNodes - 1;
+                listOfSectorsInArea.add( new Sector(startNodeId, endNodeId));
+            }
+        }
+        return listOfSectorsInArea;
     }
 }
