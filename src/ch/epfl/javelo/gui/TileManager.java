f3402be14ae2e9c9e1fdcdcb9b2895ba60f8439c
Loris Verga <loris.verga@epfl.ch>
2022-04-17 17:17:41 +0200

    début partie 7

---
+++ src/ch/epfl/javelo/gui/TileManager.java
@@ -0,0 +1,142 @@
+package ch.epfl.javelo.gui;
+
+import javafx.scene.image.Image;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * La classe TileManager, publique et finale, représente un gestionnaire de tuiles OSM. Son rôle est d'obtenir
+ * les tuiles depuis un serveur de tuiles et de les stocker dans un cache mémoire et dans un cache disque.
+ */
+public final class TileManager {
+
+
+    private final Path path;
+    private final String tileServerName;
+    private final LinkedHashMap<TileId, Image> cacheMemory;
+
+
+    public TileManager(Path path, String tileServerName){
+        this.path = path;
+        this.tileServerName = tileServerName;
+        this.cacheMemory = new LinkedHashMap<TileId, Image>(100, 0.75F, true); //TODO check
+    }
+
+
+
+    /**
+     * La méthode imageForTileAt prend en argument l'identité d'une tuile et retourne son image.
+     * L'image est cherchée tout d'abord dans le cache mémoire et si elle s'y trouve est retournée.
+     * Si elle ne s'y trouve pas elle est cherchée dans le cache disque. Si elle s'y trouve elle
+     * est retournée et placée dans le cache mémoire. Si elle ne s'y trouve pas elle est cherchée
+     * sur le serveur, retournée et placée dans les mémoires.
+     * @param tileId
+     * @return
+     */
+    public Image imageForTileAt(TileId tileId){
+
+        //Retourne l'image si elle se trouve dans la mémoire cache.
+        if (cacheMemory.containsKey(tileId)){
+            return cacheMemory.get(tileId);
+        }
+
+        //Cherche l'image dans la mémoire disque.
+        int zoomLevel = tileId.zoomLevel();
+        int indexX = tileId.indexX();
+        int indexY = tileId.indexY();
+        Path pathOfImage = path.resolve(Integer.toString(zoomLevel)).
+                resolve(Integer.toString(indexX)).
+                resolve(Integer.toString(indexY));
+
+        if (Files.exists(pathOfImage)){
+            try (InputStream imageStream = Files.newInputStream(pathOfImage)) {
+                Image image = new Image(imageStream);
+                addImageCacheMemory(image, tileId);
+                return image;
+            }
+            catch(IOException e){
+                //TODO what to do ?
+            }
+        }
+        else{
+
+        }
+
+
+
+
+
+
+
+        return null;
+    }
+
+
+    private void addImageCacheMemory(Image image, TileId tileId){
+        if (cacheMemory.size()<100){
+            cacheMemory.put(tileId, image);
+        }
+        else{ //TODO check if it's correct
+            Iterator<Map.Entry<TileId, Image>> iterator = cacheMemory.entrySet().iterator();
+            TileId tileIdOfOldestAccessedElement = iterator.next().getKey();
+            cacheMemory.remove(tileIdOfOldestAccessedElement);
+            cacheMemory.put(tileId, image);
+        }
+    }
+
+
+
+
+
+
+
+
+    /**
+     * L'enregistrement TileId, imbriqué dans la classe TileManager, représente l'identité d'une tuile OSM.
+     * Il possède trois attributs :
+     * @param zoomLevel le niveau de zoom de la tuile,
+     * @param indexX l'index X de la tuile,
+     * @param indexY et l'index Y de la tuile.
+     *
+     * @author Loris Verga (345661)
+     */
+    public record TileId(int zoomLevel, int indexX, int indexY) {
+
+        /**
+         * Le constructeur compact de l'enregistrement TileId lève une exception si les arguments ne sont pas valides.
+         *
+         * @param zoomLevel le niveau de zoom de la tuile,
+         * @param indexX    l'index X de la tuile,
+         * @param indexY    et l'index Y de la tuile.
+         */
+        public TileId(int zoomLevel, int indexX, int indexY) {
+            if (!isValid(zoomLevel, indexX, indexY)) {
+                throw new IllegalArgumentException();
+            }
+            this.zoomLevel = zoomLevel;
+            this.indexX = indexX;
+            this.indexY = indexY;
+        }
+
+        /**
+         * La méthode isVaild retourne vrai si et seulement si les trois arguments sont valides.
+         *
+         * @param zoomLevel le niveau de zoom de la tuile.
+         * @param indexX    l'index X de la tuile.
+         * @param indexY    l'index Y de la tuile.
+         * @return vrai si les arguments sont valides.
+         */
+        public boolean isValid(int zoomLevel, int indexX, int indexY) {
+            return zoomLevel >= 0 && //TODO le prof a dit depuis piazza de ne pas vérifier upperbound de zoomlevel
+                    indexX >= 0 &&
+                    indexY >= 0; //TODO Check if there exist an upper bound
+        }
+    }
+}
