997547443df44c8387f2c0b321366273102a9610
Juanika-chr <juan.iaconucci@gmail.com>
2022-04-20 20:55:20 +0200

    GpxGenerator finished and commented

--- src/ch/epfl/javelo/routing/GpxGenerator.java
+++ src/ch/epfl/javelo/routing/GpxGenerator.java
@@ -1,9 +1,5 @@
 package ch.epfl.javelo.routing;
 
-
-
-
-
 import ch.epfl.javelo.projection.PointCh;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -14,8 +10,6 @@ import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.*;
-import java.util.function.DoubleUnaryOperator;
-
 
 /**
  * La classe GpxGenerator représente un générateur d'itinéraire au format GPX.
@@ -25,11 +19,18 @@ import java.util.function.DoubleUnaryOperator;
 public class GpxGenerator {
     private GpxGenerator() {}
 
-    //todo not sure if profil here is an elevationProfil or a DoubbleUnaryOperator..
+    /**
+     * La méthode createGpx permet de créer un document de type GPX.
+     * @param route l'itinéraire que l'on veut convertir en GPX
+     * @param profile le profile associer à l'itinéraire
+     * @return le document GPX.
+     */
     public static Document createGpx(Route route, ElevationProfile profile){
 
+        //On crée la base du document GPX.
         Document doc = newDocument();
 
+        //On crée l'élément root, un enfant de doc, et on initialise ces attributs.
         Element root = doc
                 .createElementNS("http://www.topografix.com/GPX/1/1",
                         "gpx");
@@ -43,19 +44,27 @@ public class GpxGenerator {
         root.setAttribute("version", "1.1");
         root.setAttribute("creator", "JaVelo");
 
+        //On crée l'élément metadata, un enfant de root.
         Element metadata = doc.createElement("metadata");
         root.appendChild(metadata);
 
+        //On crée l'élément name, un enfant de metadata,
+        //qui a comme text brute "Route Javelo".
         Element name = doc.createElement("name");
         metadata.appendChild(name);
         name.setTextContent("Route JaVelo");
 
-
+        //On crée l'élément rte (route), enfant de root.
         Element rte = doc.createElement("rte");
         root.appendChild(rte);
 
         PointCh lastPoint = route.points().get(0);
         double currentDistance = 0.0;
+
+        //Pour chaque point sur l'itinéraire,
+        //on crée l'élément rtept (routePoint), enfant de rte,
+        //qui contient les attributs lat (la latitude) et long (la longitude) du point,
+        //et a comme enfant l'élément ele qui correspond à l'altitude à ce point.
         for(PointCh point: route.points()){
             Element rtept = doc.createElement("rtept");
             rte.appendChild(rtept);
@@ -66,16 +75,21 @@ public class GpxGenerator {
             rtept.appendChild(ele);
             currentDistance += lastPoint.distanceTo(point);
             ele.setTextContent(String.valueOf(profile.elevationAt(currentDistance)));
-            //todo I think you have to put this as child
             lastPoint = point;
 
         }
         return doc;
     }
 
-    public static void writeGpx(String fileName, Route route, ElevationProfile profil){
+    /**
+     * La méthode writeGpx permet d'écrire un document gpx sur un fichier donné.
+     * @param fileName le fichier donné
+     * @param route l'itinéraire que l'on veut convertir en GPX
+     * @param profile le profile associer à l'itinéraire
+     */
+    public static void writeGpx(String fileName, Route route, ElevationProfile profile){
 
-        Document doc = createGpx(route, profil);
+        Document doc = createGpx(route, profile);
 
         try(FileOutputStream outputStream = new FileOutputStream(fileName);
 
@@ -99,6 +113,10 @@ public class GpxGenerator {
         }
     }
 
+    /**
+     * La méthode private newDocument permet de faciliter la création de Document.
+     * @return un document.
+     */
     private static Document newDocument() {
         try {
             return DocumentBuilderFactory
