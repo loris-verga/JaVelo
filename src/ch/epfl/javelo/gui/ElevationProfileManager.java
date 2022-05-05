cf7e63778bb3cc73a05ec837688c6dd82414955d
Loris Verga <loris.verga@epfl.ch>
2022-05-05 16:49:24 +0200

    ElevationProfileManager.java v2

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -1,8 +1,12 @@
 package ch.epfl.javelo.gui;
 
 import ch.epfl.javelo.routing.ElevationProfile;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 /**
  * La classe ElevationProfileManager gère l'affichage et l'interaction avec le profil
@@ -12,6 +16,10 @@ public final class ElevationProfileManager {
 
     private final ReadOnlyObjectProperty<ElevationProfile> elevationProfileProperty;
     private final ReadOnlyDoubleProperty highlightedPositionProperty;
+
+    private final Pane borderPane;
+
+    private final DoubleProperty mousePositionOnProfileProperty;
 
     /**
      * Le constructeur de la classe prend en argument :
@@ -29,5 +37,32 @@ public final class ElevationProfileManager {
         this.elevationProfileProperty = elevationProfile;
         this.highlightedPositionProperty = highlightedPosition;
 
+        this.borderPane = new BorderPane();
+
+        this.mousePositionOnProfileProperty = new SimpleDoubleProperty(Double.NaN);
+
     }
+
+    /**
+     * La méthode pane retourne le panneau contenant le dessin du profil.
+     * @return un Pane JavaFX.
+     */
+    public Pane pane(){
+        return this.borderPane;
+    }
+
+
+    /**
+     * La méthode mousePositionOnProfileProperty retourne une propriété en lecture seule
+     * contenant la position du pointeur de la souris le long du profil(en mètres, arrondie
+     * à l'entier le plus proche), ou NaN si le pointeur de la souris ne se trouve pas
+     * au-dessus du profil.
+     * @return
+     */
+    public ReadOnlyDoubleProperty mousePositionProfileProperty(){
+        return this.mousePositionOnProfileProperty; //TODO check que la la valeur soit arrondie à l'entier le plus proche.
+    }
+
+
+
 }
