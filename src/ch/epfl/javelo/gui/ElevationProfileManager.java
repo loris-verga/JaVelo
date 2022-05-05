18960cf12c1aa9f71f14f2d196cbc0418a4b4659
Juanika-chr <juan.iaconucci@gmail.com>
2022-05-06 01:16:25 +0200

    until step 4 for step 10 finished

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -1,19 +1,18 @@
 package ch.epfl.javelo.gui;
 
 import ch.epfl.javelo.routing.ElevationProfile;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.ReadOnlyDoubleProperty;
-import javafx.beans.property.ReadOnlyObjectProperty;
-import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.*;
 import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.Polygon;
+import javafx.scene.shape.*;
 import javafx.scene.text.Text;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.NonInvertibleTransformException;
 import javafx.scene.transform.Transform;
 
 /**
@@ -46,8 +45,15 @@ public final class ElevationProfileManager {
     private final Text textVbox;
 
     private final Insets inset = new Insets(10, 10, 20, 40); //TODO utiliser pour part 2
-    private final Transform transform; //TODO créer cette tranformation
 
+    private ObjectProperty<Rectangle2D> blueRectangleProperty;
+    private ObjectProperty<Transform> screenToWorldProperty;
+    private ObjectProperty<Transform> worldToScreenProperty;
+
+    private static final int[] POSITION_STEPS =
+            { 1000, 2000, 5000, 10_000, 25_000, 50_000, 100_000 };
+    private static final int[] ELEVATION_STEPS =
+            { 5, 10, 20, 25, 50, 100, 200, 250, 500, 1_000 };
 
     /**
      * Le constructeur de la classe prend en argument :
@@ -92,7 +98,6 @@ public final class ElevationProfileManager {
         borderPane.setBottom(vBox);
         textVbox = new Text();
         vBox.getChildren().add(textVbox);
-
     }
 
     /**
@@ -115,9 +120,94 @@ public final class ElevationProfileManager {
         return this.mousePositionOnProfileProperty; //TODO check que la la valeur soit arrondie à l'entier le plus proche.
     }
 
+    private void createTransformationsProperty() throws NonInvertibleTransformException {
 
+        Affine screenToWorld = new Affine();
 
+        double tx = - inset.getLeft();
+        double ty = - inset.getTop();
+        screenToWorld.prependTranslation( tx, ty);
 
+        tx = blueRectangleProperty.get().getWidth()
+                / (pane.getWidth() - (inset.getLeft() + inset.getRight()));
+        ty = - blueRectangleProperty.get().getHeight()
+                /(pane.getHeight() - (inset.getTop() + inset.getBottom()));
+        screenToWorld.prependScale( tx, ty);
 
+        tx = blueRectangleProperty.get().getMinX();
+        ty = blueRectangleProperty.get().getMaxY();
+        screenToWorld.prependTranslation( tx, ty);
 
+        screenToWorldProperty.set(screenToWorld);
+        worldToScreenProperty.set(screenToWorld.createInverse());
+    }
+
+    private void createBlueRectangleProperty(){
+        blueRectangleProperty = new SimpleObjectProperty<>();
+
+        double minX = 0;
+        double minY = elevationProfileProperty.get().minElevation();
+        double width = elevationProfileProperty.get().length();
+        double height = elevationProfileProperty.get().maxElevation() - elevationProfileProperty.get().minElevation();
+        blueRectangleProperty.set(new Rectangle2D(minX, minY, width, height));
+    }
+
+    private void createGrid(){
+        path.getElements().clear();
+
+        double pixelsBetweenVerticalLines;
+        int verticalStep = 0;
+
+        double pixelsBetweenHorizontalLines;
+        int horizontalStep = 0;
+
+        for(int i = 0; i < POSITION_STEPS.length; i++) {
+            Point2D point = worldToScreenProperty.get()
+                    .deltaTransform(
+                            new Point2D(POSITION_STEPS[i], 0));
+            pixelsBetweenVerticalLines = point.getX();
+            if(pixelsBetweenVerticalLines < 50){
+                verticalStep = POSITION_STEPS[i];
+                break;
+            }
+        }
+
+        for(int i = 0; i < ELEVATION_STEPS.length; i++) {
+            Point2D point = worldToScreenProperty.get()
+                                .deltaTransform(
+                                    new Point2D(0,ELEVATION_STEPS[i]));
+            pixelsBetweenHorizontalLines = point.getY();
+            if(pixelsBetweenHorizontalLines < 25){
+                horizontalStep = POSITION_STEPS[i];
+                break;
+            }
+        }
+
+        int numberOfVerticalLines = (int) blueRectangleProperty.get().getWidth() / verticalStep;
+        int numberOfHorizontalLines = (int)blueRectangleProperty.get().getHeight() / horizontalStep;
+
+        for(int i = 1; i <= numberOfVerticalLines; i++){
+            Point2D point2DMoveTo = worldToScreenProperty.get()
+                    .transform(i * verticalStep ,blueRectangleProperty.get().getMinY());
+            PathElement moveTo = new MoveTo(point2DMoveTo.getX(),point2DMoveTo.getY());
+
+            Point2D point2DLineTo = worldToScreenProperty.get()
+                    .transform(i * verticalStep ,blueRectangleProperty.get().getMaxY());
+            PathElement lineTo = new LineTo(point2DLineTo.getX(), point2DLineTo.getY());
+
+            path.getElements().addAll(moveTo,lineTo);
+        }
+
+        for(int i = 1; i <= numberOfHorizontalLines; i++){
+            Point2D point2DMoveTo = worldToScreenProperty.get()
+                    .transform(blueRectangleProperty.get().getMinX(), i * horizontalStep);
+            PathElement moveTo = new MoveTo(point2DMoveTo.getX(), point2DMoveTo.getY());
+
+            Point2D point2DLineTo = worldToScreenProperty.get()
+                    .transform(blueRectangleProperty.get().getMaxX(), i * horizontalStep);
+            PathElement lineTo = new LineTo(point2DLineTo.getX(), point2DLineTo.getY());
+
+            path.getElements().addAll(moveTo,lineTo);
+        }
+    }
 }
