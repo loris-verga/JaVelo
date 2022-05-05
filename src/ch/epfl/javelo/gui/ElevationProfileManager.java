85d23d26ff32c915747cf2bf76f740e7453d3b7b
Loris Verga <loris.verga@epfl.ch>
2022-05-05 17:24:49 +0200

    ElevationProfileManager fin partie 1

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -5,8 +5,14 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.Group;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
 
 /**
  * La classe ElevationProfileManager gère l'affichage et l'interaction avec le profil
@@ -17,9 +23,26 @@ public final class ElevationProfileManager {
     private final ReadOnlyObjectProperty<ElevationProfile> elevationProfileProperty;
     private final ReadOnlyDoubleProperty highlightedPositionProperty;
 
-    private final Pane borderPane;
+    private final BorderPane borderPane;
 
     private final DoubleProperty mousePositionOnProfileProperty;
+
+    //Le panneau sert à organiser ses enfants.
+    private final Pane pane;
+    //Le chemin représente la grille.
+    private final Path path;
+    //le groupe contient les étiquettes textuelles de la grille.
+    private final Group group;
+    //Le polygone représente le graphe du profil.
+    private final Polygon polygon;
+    //La ligne représente la position en évidence.
+    private final Line line;
+
+    //TODO add text
+
+    private final VBox vBox;
+    private final Text textVbox;
+
 
     /**
      * Le constructeur de la classe prend en argument :
@@ -37,9 +60,30 @@ public final class ElevationProfileManager {
         this.elevationProfileProperty = elevationProfile;
         this.highlightedPositionProperty = highlightedPosition;
 
-        this.borderPane = new BorderPane();
 
         this.mousePositionOnProfileProperty = new SimpleDoubleProperty(Double.NaN);
+
+        //Initialisation des nœuds :
+        this.borderPane = new BorderPane();
+        borderPane.getStylesheets().add("elevation_profile.css");
+
+        this.pane = new Pane();
+        borderPane.setCenter(pane);
+
+        this.path = new Path();
+        path.setId("grid");
+        pane.getChildren().add(path);
+        this.group = new Group();
+        pane.getChildren().add(group);
+        this.polygon = new Polygon();
+        this.polygon.setId("profile");
+        pane.getChildren().add(polygon);
+
+        this.vBox = new VBox();
+        vBox.setId("profile_data");
+        borderPane.setBottom(vBox);
+        textVbox = new Text();
+        vBox.getChildren().add(textVbox);
 
     }
 
@@ -62,6 +106,8 @@ public final class ElevationProfileManager {
     public ReadOnlyDoubleProperty mousePositionProfileProperty(){
         return this.mousePositionOnProfileProperty; //TODO check que la la valeur soit arrondie à l'entier le plus proche.
     }
+
+
 
 
 
